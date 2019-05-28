1200fe4b97021b38461160ce82dbcca8e4438ceb
Thomas Cartotto <34584458+Thomas-Cartotto@users.noreply.github.com>
2019-05-28 18:38:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ router.post('/hitgong', async (ctx,next) => {
   });
   
   //var messageArray = ['Owch! That hurt...', 'Again?? My arm is tired', 'Im not made of metal you know...', '', ''];
-  ctx.throw(200, ctx.request.body);
+  ctx.throw(200, String(ctx.params));
 });
 
 const PORT = process.env.PORT || 5001;
